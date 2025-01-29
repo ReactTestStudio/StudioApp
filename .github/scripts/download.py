85596111eb7631d3c5831ad884abf4d97bb140e7
Pipe <devops@esmorga.canarte.org>
2025-01-29 13:25:47 +0100

    Create Path

--- .github/scripts/download.py
+++ .github/scripts/download.py
@@ -6,7 +6,6 @@ from googleapiclient.http import MediaIoBaseDownload
 import io
 import os
 
-# Autenticación
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 SERVICE_ACCOUNT_FILE = 'service_account.json'
 
@@ -15,7 +14,6 @@ creds = service_account.Credentials.from_service_account_file(
 service = build('drive', 'v3', credentials=creds)
 file_name = 'app-release.apk'
 
-# Buscar el archivo por nombre
 results = service.files().list(
     q=f"name = '{file_name}'",
     fields="files(id, name)"
@@ -24,20 +22,22 @@ results = service.files().list(
 files = results.get('files', [])
 
 if not files:
-    print(f"No se encontró el archivo con el nombre '{file_name}'.")
+    print(f"Not Found '{file_name}'.")
 else:
-    file_id = files[0]['id']  # Obtener el ID del primer archivo que coincida
-    print(f"Archivo encontrado: {file_name}, ID: {file_id}")
-    
-    # Descargar el archivo
+    file_id = files[0]['id']  
+    print(f"Found: {file_name}, ID: {file_id}")
+
+    path = './android/app/build/outputs/apk/release/'
+    os.makedirs(path, exist_ok=True)
+
     request = service.files().get_media(fileId=file_id)
-    fh = io.FileIO('./android/app/build/outputs/apk/release/'+file_name, 'wb')
+    fh = io.FileIO(path+file_name, 'wb')
 
     downloader = MediaIoBaseDownload(fh, request)
     done = False
     while done is False:
         status, done = downloader.next_chunk()
-        print(f'Descargando {int(status.progress() * 100)}%.')
+        print(f'Downloading ... {int(status.progress() * 100)}%.')
     
-    print('Archivo descargado con éxito!')
+    print('Success!')
           
